b15330c7c6a054c5095b15d579f0b08391ede731
Oscar Ruiz <oscarruiz2614@gmail.com>
2024-10-21 16:14:22 -0500

    :tada: init project

---
+++ src/main/java/com/example/inventoryDemo/InventoryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventoryDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryDemoApplication.class, args);
+	}
+
+}
